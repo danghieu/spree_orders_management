cc3d41fedd99082cbc45575697da3d68347954d5
Dang Hieu <danghieu193@gmail.com>
2016-04-12 15:24:45 +0700

    a

--- app/assets/javascripts/spree/backend/spree_orders_management.js
+++ app/assets/javascripts/spree/backend/spree_orders_management.js
@@ -1,3 +1,30 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
-//= require spree/backend/shipments.js
+// handle shipping method save
+  $('[data-hook=admin_shipment_form] a.save-method').on('click', function (event) {
+    event.preventDefault();
+
+    var link = $(this);
+    var shipment_number = link.data('shipment-number');
+    var selected_shipping_rate_id = link.parents('tbody').find("select#selected_shipping_rate_id[data-shipment-number='" + shipment_number + "']").val();
+    var selected_shipping_rate_date_delivery = link.parents('tbody').find("input[name='selected_shipping_rate_date_delivey[]'][data-shipment-number='" + shipment_number + "']").val();
+    var unlock = link.parents('tbody').find("input[name='open_adjustment'][data-shipment-number='" + shipment_number + "']:checked").val();
+    var url = Spree.url(Spree.routes.shipments_api + '/' + shipment_number + '.json');
+
+    $.ajax({
+      type: 'PUT',
+      url: url,
+      data: {
+        shipment: {
+          selected_shipping_rate_id: selected_shipping_rate_id,
+          date_delivery:selected_shipping_rate_date_delivery,
+          unlock: unlock
+        },
+        token: Spree.api_key
+      }
+    }).done(function () {
+      window.location.reload();
+    }).error(function (msg) {
+      console.log(msg);
+    });
+  });
