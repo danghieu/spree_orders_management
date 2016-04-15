954dff92b686ee9cfb673f4dd8f2b1283ddbff07
Dang Hieu <danghieu193@gmail.com>
2016-04-15 10:47:45 +0700

    a

--- app/controllers/spree/admin/payments_controller_decorator.rb
+++ app/controllers/spree/admin/payments_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Admin::PaymentsController.class_eval do
  def load_data
         @amount = params[:amount] || load_order.total
-        @payment_methods = PaymentMethod.available_on_back_end
+        @payment_methods = Spree::PaymentMethod.available(:back_end)
         if @payment and @payment.payment_method
           @payment_method = @payment.payment_method
         else
